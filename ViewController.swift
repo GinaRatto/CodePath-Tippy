f9e94391318ee29a37e765086792619acab67022
GinaRatto <ginagratto@gmail.com>
2016-12-11 13:50:32 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Gina Ratto on 11/17/16.
+//  Copyright © 2016 Gina Ratto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var personStepper: UIStepper!
+    @IBOutlet weak var personLabel: UILabel!
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+        
+        let bottomColor = UIColor(red: (15/255.0), green: (118/255.0), blue: (128/255.0), alpha: 1)
+        let topColor = UIColor(red: (84/255.0), green: (187/255.0), blue: (187/255.0), alpha: 1)
+        
+        let gradientColors: [CGColor] = [topColor.cgColor, bottomColor.cgColor]
+        let gradientLocations: [NSNumber] = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations
+        
+        gradientLayer.frame = self.view.bounds
+        self.view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onTap(_ sender: Any) {
+        //view.endEditing(true)
+    }
+    
+
+    @IBAction func CalculateTip(_ sender: Any) {
+
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let people = personStepper.value
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let yourTotal = total / people
+        let persons = Int(people)
+        
+
+        personLabel.text = String(format: "Number of People: %d", persons)
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", yourTotal)
+    }
+
+
+
+}
+
